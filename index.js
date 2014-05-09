78a5ffc7546ac3fb045e7092965dbb02519c351c
thuld <thuld@outlook.com>
2014-05-09 16:17:08 +0200

    basis function

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var Promise = require('bluebird');
+
+function getById(entity, id, columns) {
+    
+}
+
+function getByAttribute(entity, attribute, value, columns){
+
+}
+
+function fetch(fetchxml){
+
+}
+
+function fetchAll(fetchxml){
+
+}
+
+export.getById = getById;
+export.getByAttribute = getByAttribute;
+export.fetch = fetch;
+export.fetchAll = fetchAll;
