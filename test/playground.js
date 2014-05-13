f1385e6905928fb0c6d5b31bf31890b7981a81ba
thuld <thuld@outlook.com>
2014-05-13 14:30:37 +0200

    Somoe more tests

--- test/playground.js
+++ test/playground.js
@@ -1,6 +1,7 @@
 var libxmljs = require('libxmljs');
 var fs = require('fs');
 
+/*
 var body = fs.readFileSync( './message.xml', 'utf8');
 
 // console.log(body);
@@ -83,3 +84,83 @@ var tokenResp = xmldoc.get('//S:Body/wst:RequestSecurityTokenResponse', ns)
 var t = tokenResp.get('//wst:RequestedAttachedReference/wsse:SecurityTokenReference/wsse:KeyIdentifier', ns).text();
 
 console.log(t);
+*/
+
+
+var ns = {
+        'S': "http://www.w3.org/2003/05/soap-envelope",
+        'wst': "http://schemas.xmlsoap.org/ws/2005/02/trust",
+        'wsse': "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd",
+        'wsu': "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd",
+        'saml': "urn:oasis:names:tc:SAML:1.0:assertion",
+        'wsp': "http://schemas.xmlsoap.org/ws/2004/09/policy",
+        'psf': "http://schemas.microsoft.com/Passport/SoapServices/SOAPFault",
+        'ds':"http://www.w3.org/2000/09/xmldsig#"
+    },
+    responseBody = fs.readFileSync('./SoapAuthResponceMessage.xml', 'utf8')
+    xmldoc = libxmljs.parseXml( responseBody ),
+    tokenResponse = xmldoc.get('/S:Envelope/S:Body/wst:RequestSecurityTokenResponse', ns),
+    securityToken = tokenResponse.get('./wst:RequestedSecurityToken', ns),
+    keyIdentifer = tokenResponse.get('./wst:RequestedAttachedReference/wsse:SecurityTokenReference/wsse:KeyIdentifier', ns).text(),
+    encryptedData = securityToken.get('./*[local-name()="EncryptedData"]');
+
+console.log('++++++++++++++++++++++++++++++++++++++++++');
+console.log( keyIdentifer );
+console.log('++++++++++++++++++++++++++++++++++++++++++');
+
+console.log('-------------------------------------------------------------');
+
+var xml =  '<?xml version="1.0" encoding="UTF-8"?>' +
+           '<root>' +
+              '<someone foo="johndoe">' +
+                  '<child foo="child-of-someone">' +
+                  '</child>' +
+               '</someone>' +
+               '<child foo="bar">' +
+                   '<grandchild baz="fizbuzz">grandchild content</grandchild>' +
+               '</child>' +
+               '<child foo="last-child-in-doc">' +
+               '</child>' +
+               '<sibling>with content!</sibling>' +
+           '</root>';
+
+var xmldoc = libxmljs.parseXml(xml);
+var root = xmldoc.get('/root');
+var child_a = xmldoc.get('/root/child');
+// hier wollen wir nur das ersten Kind vom parent laden nihct alle
+var child_b = root.get('./child');
+var child_c = xmldoc.get('./child');
+// das ist der falsche ansatz, es nicht das erste element irgendwo unter "root"
+var child_d = xmldoc.get('//child');
+
+console.log( root.text() );
+console.log( 'child-a: ' + child_a.attr('foo').value() );
+console.log( 'child-b: ' + child_b.attr('foo').value() );
+console.log( 'child-c: ' + child_c.attr('foo').value() );
+console.log( 'child-d: ' + child_d.attr('foo').value() );
+
+console.log('-------------------------------------------------------------');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test/specification.Authentication.js
@@ -0,0 +1,101 @@
+'use strict';
+/*globals describe, it, beforeEach, afterEach */
+
+var XrmAuth = require('./../lib/XrmAuth');
+var XrmNodeKit = require('./../index.js');
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var request = require('request');
+var fs = require('fs');
+
+// global test values /parameter
+var url = "https://alfapeopleag.crm4.dynamics.com/";
+var username = "thul@AlfaPeopleAG.onmicrosoft.com";
+var password = "foobar0815%";
+
+function fakePostAuthRequest( options, callback ){
+
+    var error = null,
+        response = { statusCode: 200 },
+        body = fs.readFileSync('./test/SoapAuthResponceMessage.xml', 'utf8');
+
+    callback(error, response, body);
+}
+
+describe('Integration Tests for the auth module of the XrmNodeKit', function(){
+
+    var sandbox = null;
+    
+    beforeEach( function(){
+        // arrange - create a sandbox for the stubs
+        sandbox = sinon.sandbox.create();
+
+    });
+
+    afterEach(function(){
+        sandbox.restore();
+    });
+
+    it('should parse the soap-auth-request', function( done ){
+
+        // arrange - sub for the request constructor
+        sandbox.stub(request, 'post', fakePostAuthRequest);
+
+        // action
+        XrmAuth.getAuthData( 'fake-url', 'fake-user', 'fake-pwd' ).then(function( authdetails ){
+
+            // console.log(authdetails.keyIdentifer);
+            // assert
+            expect( authdetails.keyIdentifer ).to.be.equal('VrPTiMUGicDu5XJqdzD6SLXa8fY=');
+
+            expect( authdetails.tokens ).to.be.an('object');
+            expect( authdetails.tokens.one ).to.be.equal('fake-CipherValue-one');
+            expect( authdetails.tokens.two ).to.be.equal('fake-CipherValue-two');
+
+            done();
+        });
+    });
+
+    it('should generate an soap-header', function( done ){ 
+
+        // arrange - sub for the request constructor
+        sandbox.stub(request, 'post', fakePostAuthRequest);
+
+        // arrange - define the expected values
+        var expected1 = '<CipherValue>fake-CipherValue-one</CipherValue>';
+        var expected2 = '<CipherValue>fake-CipherValue-two</CipherValue>';
+
+        // action 
+        XrmAuth.getHeaderOnline(url, username, password).then(function( header ){
+
+            // assert
+            expect( header ).to.have.string( expected1 );
+            expect( header ).to.have.string( expected2 );
+
+            done();
+        });
+    });
+});
+
+describe('Integration: The module supports differnt messages', function(){
+
+    this.timeout(10000);
+
+    it('should fetch the name of the current user', function(done){
+
+        // arrange - get the authentication header
+        XrmAuth.getHeaderOnline(url, username, password).then(function( header ){
+
+            // arrange 
+            var xrmNodeKit = new XrmNodeKit(url, header);
+
+            // action
+            xrmNodeKit.whoAmI().then(function(whoAmI){
+
+                expect(whoAmI.UserName).to.be.equal('Daniel Thul');
+
+                done();
+            });
+        });
+    });
+});
